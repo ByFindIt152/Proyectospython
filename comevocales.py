84f1ca0bd14e3dda2ae71ac6d12393583d056f0e
ByFindIt152 <79736864+ByFindIt152@users.noreply.github.com>
2022-07-04 22:53:05 -0500

    Create comevocales.py

---
+++ comevocales.py
@@ -0,0 +1,16 @@
+# Indicar al usuario que ingrese una palabra
+# y asignarlo a la variable user_word.
+user_word = input("Ingresa una palabra").upper()
+for letter in user_word:
+    # Completa el cuerpo del bucle for.
+    if letter == "A":
+        continue
+    elif letter == "E":
+        continue
+    elif letter == "I":
+        continue
+    elif letter == "O":
+        continue
+    elif letter == "U":
+        continue
+    print(letter)
